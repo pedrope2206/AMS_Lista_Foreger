e8db108e31eed40b5f16caafc4658b10085b157c
pedrope2206 <pivettapedro93@gmail.com>
2021-09-28 19:12:16 -0300

    AMS_Lista_Foreger

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AMS_Lista_Foreger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          string[] produtos = new string[5];
+          {
+            produtos [0] = "Arroz";
+            produtos [1] = "feijão";
+            produtos [2] = "Leite";
+            produtos [3] = "Açucar";
+            produtos [4] = "Macarrão";
+          
+            foreach(string mercado in produtos)
+            {
+
+              Console.WriteLine(mercado);
+
+            }
+          }
+
+          
+        }
+    }
+}
